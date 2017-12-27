1d1f733007336067449753cb962416cfbe5fa3d3
shenyefeng <shenyefeng@yesheng-sh.com>
2017-12-27 17:17:47 +0800

    up

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -10,7 +10,62 @@ const store = new Vuex.Store({
     logo: 'syf',
     options: [],
     activeIndex: '',
-    userInfo: {}
+    userInfo: {},
+    routes: [
+      {
+        path: '/login',
+        name: '登录',
+        parpath: "login",
+      },
+      {
+        path: '/dashboard',
+        name: '首页',
+        redirect: '/dashboard',
+        leaf: true, // 只有一个节点
+        menuShow: true,
+        iconCls: 'iconfont icon-home', // 图标样式class
+        parpath: "dashboard",
+        children: [
+          {path: '/dashboard', iconCls: 'iconfont icon-home', name: '首页',ishide: true}
+        ]
+      },
+      {
+        path: '/',
+        name: '资产管理',
+        menuShow: true,
+        leaf: true, // 只有一个节点
+        iconCls: 'iconfont icon-users', // 图标样式class
+        parpath: "asset",
+        children: [
+          {path: '/asset/list', iconCls: 'iconfont icon-users', name: '资产列表', parname: '资产管理'}
+        ]
+      },
+      {
+        path: '/',
+        name: '图书管理',
+        menuShow: true,
+        leaf: true, // 只有一个节点
+        iconCls: 'iconfont icon-books',
+        parpath: "book",
+        children: [
+          {path: '/book/list', iconCls: 'iconfont icon-books', name: '图书列表', parname: '图书管理'},
+          {path: '/book/category', iconCls: 'iconfont icon-books', name: '图书分类', parname: '图书管理'}
+        ]
+      },
+      {
+        path: '/',
+        name: '设置',
+        menuShow: true,
+        leaf: true, // 只有一个节点
+        iconCls: 'iconfont icon-setting1',
+        parpath: "user",
+        children: [
+          {path: '/user/profile', iconCls: 'iconfont icon-setting1', name: '个人信息', parname: '设置'},
+          {path: '/user/changepwd', iconCls: 'iconfont icon-setting1', name: '修改密码', parname: '设置'}
+        ]
+      }
+    ],
+    menuchildren: []
   },
   mutations: {
     newAuthor (state, msg) {
@@ -38,6 +93,10 @@ const store = new Vuex.Store({
     // 设置详情信息
     save_detail_info (state, info) {
       this.state.userInfo = info;
+    },
+    // 设置二级菜单
+    set_active_menuchildren (state, menudata) {
+      this.state.menuchildren = menudata;
     }
   }
 })
